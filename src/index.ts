b50528e9357bba0c55c42b36eac5746edb539482
facundom <facundomiglierini@gmail.com>
2025-03-16 15:06:51 -0300

    Added MongoDB connection

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
-import express, { Request, Response } from 'express';
+import http from 'http';
+import express from 'express';
+import mongoose from 'mongoose';
+
+import router from './routes';
+import {server, mongo} from './config/config';
+
 
 const app: express.Application = express();
+export let httpServer: ReturnType<typeof http.createServer>;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hola');
-});
+export const Main = async () => {
 
-app.listen(3500, () => {
-  console.log('Listening on http://localhost:3500');
-});
+  app.use(express.json());
+
+  try {
+    const connection = await mongoose.connect(mongo.MONGO_CONNECTION, mongo.MONGO_OPTIONS);
+    console.log('Connected to Mongo: ', connection.version);
+  } catch (error) {
+    console.log('Unnable to connect to Mongo')
+    console.error(error)
+  }
+
+  app.use(router);
+
+  httpServer = http.createServer(app);
+  httpServer.listen(server.SERVER_PORT, () => {
+    console.log(`Server started on ${server.SERVER_HOSTNAME}:${server.SERVER_PORT}`);
+  })
+}
+
+export const Shutdown = (callback: any) => httpServer && httpServer.close(callback);
+
+Main();
